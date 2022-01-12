fe2ba2b6b42a6fa5b4d85cdec989d95c962fc11f
aagraston <aagraston@gmail.com>
2022-01-11 19:21:21 -0800

    third exercise completed all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function(str) {
+const reverseString = function (str) {
+  strReversed = ""
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    strReversed = strReversed.concat(str.charAt(i))
+  }
+
+  return strReversed;
 };
 
 // Do not edit below this line
